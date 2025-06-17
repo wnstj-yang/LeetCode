dcc94dd81951a8ab39acddfaa17e1928e55cc787
양준서(JoonSeo Yang) <77601720+wnstj-yang@users.noreply.github.com>
2025-06-17 20:50:10 +0900

    Time: 17 ms (20.27%), Space: 13.6 MB (64.67%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -7,6 +7,7 @@ class Solution(object):
                 nums_cnt[num] = 1
             else:
                 nums_cnt[num] += 1
+            # majority element는 하나 꼭 있기에 발견하면 종료
             if nums_cnt[num] > len(nums) // 2:
                 return num
                 
