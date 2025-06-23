31161bc22ad69197e0463838291b2c1114503654
양준서(JoonSeo Yang) <77601720+wnstj-yang@users.noreply.github.com>
2025-06-23 22:20:03 +0900

    Time: 0 ms (100%), Space: 23 MB (24.13%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        # 1. 단순 pop and insert => 시간 초과 발생 
+        # k = k % len(nums)
+        # for _ in range(k):
+        #     nums.insert(0, nums.pop())
+        ##########################################
+        
+        # 2. 나머지 연산을 통한 k값을 기준으로 붙여넣기
+        k = k % len(nums)
+        n = len(nums) - k
+        nums[:k], nums[k:] = nums[-k:], nums[:n]
+            
