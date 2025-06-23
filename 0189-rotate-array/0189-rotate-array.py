6a2d96035e4a6950c514c3e9e45ae3738dc6f71c
양준서(JoonSeo Yang) <77601720+wnstj-yang@users.noreply.github.com>
2025-06-23 22:25:40 +0900

    Time: 0 ms (100%), Space: 22.9 MB (25.98%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -14,5 +14,6 @@ class Solution(object):
         # 2. 나머지 연산을 통한 k값을 기준으로 붙여넣기
         k = k % len(nums)
         n = len(nums) - k
+        # k를 기준으로 앞과 뒤에 새로 붙여주는 리스트들을 교환
         nums[:k], nums[k:] = nums[-k:], nums[:n]
             
