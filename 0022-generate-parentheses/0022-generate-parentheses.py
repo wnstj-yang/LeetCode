88d74951444997c1d547785d1e1716992d6fe4b6
양준서(JoonSeo Yang) <77601720+wnstj-yang@users.noreply.github.com>
2025-07-20 17:17:24 +0900

    Time: 1 ms (65.26%), Space: 12.6 MB (75.18%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -2,11 +2,15 @@ class Solution(object):
     def generateParenthesis(self, n):
         ans = []
         def dfs(l, r, result):
+            # l보다 r이 크게되면 짝궁이 만들어지지 않기에 return
+            # Ex. (()( 식으로 됨
             if l < r:
                 return
+            # dfs를 돌면서 양쪽 모두 같은 경우 정답 리스트에 추가
             if l == n and r == n:
                 ans.append(result)
                 return
+            # 끝에 추가를 해야 괄호 짝궁이 만들어짐
             if l < n:
                 dfs(l + 1, r, result + '(')
             if r < n:
