dda229500a081781213b68def4800df3d28921c0
양준서(JoonSeo Yang) <77601720+wnstj-yang@users.noreply.github.com>
2025-08-03 13:50:55 +0900

    Time: 39 ms (92.52%), Space: 13.7 MB (22.32%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,10 +1,5 @@
 class Solution(object):
     def findAnagrams(self, s, p):
-        """
-        :type s: str
-        :type p: str
-        :rtype: List[int]
-        """
         result = []
         p_len = len(p)
         s_len = len(s)
@@ -12,7 +7,10 @@ class Solution(object):
         s_alpha_list = [0] * 26
         for num in p:
             p_alpha_list[ord(num) - ord('a')] += 1
-        j = 0
+
+        # 순회하면서 알파벳 위치의 개수를 카운트
+        # index가 p의 크기보다 같거나 크면 p의 크기만큼 뺀 인덱스의 알파벳 개수를 줄인다.
+        # 각 알파벳 개수가 동일하면 애나그램이므로 index위치를 저장
         for i in range(s_len):
             s_alpha_list[ord(s[i]) - ord('a')] += 1
             if i >= p_len:
@@ -20,8 +18,5 @@ class Solution(object):
                 s_alpha_list[ord(target) - ord('a')] -= 1
             if p_alpha_list == s_alpha_list:
                 result.append(i - p_len + 1)
-            # print(p_alpha_list)
-            # print(s_alpha_list)
-            # print()
         return result
         
