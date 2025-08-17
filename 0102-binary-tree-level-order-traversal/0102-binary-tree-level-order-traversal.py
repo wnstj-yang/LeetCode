632a13f569e01e8702b5fb61027240adf20ac358
양준서(JoonSeo Yang) <77601720+wnstj-yang@users.noreply.github.com>
2025-08-17 17:04:34 +0900

    Time: 4 ms (20.92%), Space: 13.4 MB (14.69%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -10,16 +10,18 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: List[List[int]]
         """
-        if root is None:
-            return []
         result = {}
-        queue = [(0, root)]
+        queue = [(0, root)] # 큐를 활용하여 순회한다.(트리의 형태만 갖춰짐)
         while queue:
             level, node = queue.pop(0)
+            if not node: # 비어있으면 끝
+                break
+            # 같은 레벨 선상의 key가 없으면 생성, 있으면 값 추가
             if level not in result:
                 result[level] = [node.val]
             else:
                 result[level].append(node.val)
+            # 각 노드의 왼쪽과 오른쪽 순서대로 체크해서 다음 단계가 존재하므로 level 증가 및 다음 노드 큐에 추가
             if node.left is not None:
                 queue.append((level + 1, node.left))
             if node.right is not None:
