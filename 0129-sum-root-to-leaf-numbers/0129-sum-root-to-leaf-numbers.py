c4c8d5618794392ac2f16cf683fc6fb0ab5a613c
양준서(JoonSeo Yang) <77601720+wnstj-yang@users.noreply.github.com>
2026-02-01 16:40:50 +0900

    Time: 0 ms (100%), Space: 12.4 MB (96.45%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
@@ -4,44 +4,27 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-# class Solution(object):
-#     def sumNumbers(self, root):
-#         global ans # 맞긴하나 설계상 이점은 아님. 전역변수라는 것이 변경될 수도 있기 때문
-#         """
-#         :type root: Optional[TreeNode]
-#         :rtype: int
-#         """
-#         ans = 0
-#         def dfs(node, result):
-#             global ans
-
-#             if node is None:
-#                 return
-#             if node.left is None and node.right is None:
-#                 ans += int(result + str(node.val))
-#                 return
-
-#             dfs(node.left, result + str(node.val))
-#             dfs(node.right, result + str(node.val))
-#         dfs(root, '')
-#         return ans
-
-
 class Solution(object):
     def sumNumbers(self, root):
+        global ans
         """
         :type root: Optional[TreeNode]
         :rtype: int
         """
+        ans = 0
         def dfs(node, result):
+            global ans
             if node is None:
-                return 0
-                
-            result = result * 10 + node.val
+                return
             if node.left is None and node.right is None:
-                return result
+                ans += int(result + str(node.val))
+                return
+            # if node.right is None:
+            #     ans += int(result + str(node.val))
+            #     return
 
-            return dfs(node.left, result) + dfs(node.right, result)
-
-        return dfs(root, 0)
+            dfs(node.left, result + str(node.val))
+            dfs(node.right, result + str(node.val))
+        dfs(root, '')
+        return ans
         
