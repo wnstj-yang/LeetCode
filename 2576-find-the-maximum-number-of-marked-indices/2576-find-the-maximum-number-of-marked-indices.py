52e36199fa190fc8f0391d8bf38a61612661364c
양준서(JoonSeo Yang) <77601720+wnstj-yang@users.noreply.github.com>
2026-02-08 15:14:01 +0900

    Time: 151 ms (12.5%), Space: 22.1 MB (12.5%) - LeetHub

--- 2576-find-the-maximum-number-of-marked-indices/2576-find-the-maximum-number-of-marked-indices.py
+++ 2576-find-the-maximum-number-of-marked-indices/2576-find-the-maximum-number-of-marked-indices.py
@@ -4,18 +4,23 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+
+        # 1. 인덱스를 한 번만 사용할 수 있다
+        # 2. 겹치면 안된다
+        # 3. 1, 2 조건 + 쌍이기 때문에 정렬하고 left는 0, right는 len(nums) // 2 시작
+        # 4. 쌍으로 구하는 것이라 2씩 더하면 될듯
         if len(nums) < 2:
             return 0
         nums.sort()
-        left, right = 0, len(nums) // 2
+        left, right = 0, 1
         ans = 0
-        # print(nums)
+
         while left < len(nums) // 2 and right < len(nums):
-            # print(left, right, nums[left] * 2, ' <= ', nums[right])
             if nums[left] * 2 <= nums[right]:
                 left += 1
-                right += 1
+                right += 1 # 이미 선택한 인덱스이기에 같이 1을 추가해주면서 진행한다
                 ans += 2
+            # 조건에 안맞으면 right의 값이 작은 것이기 때문에 큰 것을 찾아 떠난다
             else:
                 right += 1
         return ans
