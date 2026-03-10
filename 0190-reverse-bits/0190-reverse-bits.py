faa13c9d77441d914bf26904ef9234611fdeb926
양준서(JoonSeo Yang) <77601720+wnstj-yang@users.noreply.github.com>
2026-03-11 00:19:07 +0900

    Time: 7 ms (98.92%), Space: 12.3 MB (81.53%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -7,15 +7,14 @@ class Solution(object):
         binary_list = [0] * 32
         idx = 31
         while idx > 0:
-            if n % 2 == 1:
+            if n % 2 == 1: # 나머지 값이 1이면 해당 위치 이진수 값이 필요함 (Ex. 값(나머지연산 이진수) 6(0) -> 3(1) -> 1(1) : 110))
                 binary_list[idx] = 1
             n //= 2
             idx -= 1
 
-        binary_list.reverse()
-        # print(binary_list)
-        result = 0
-        binary = 1
+        binary_list.reverse() # 뒤집기
+        result = 0 # 결과물
+        binary = 1 # 현재 위치의 이진수 값
         for i in range(31, -1, -1):
             if binary_list[i] == 1:
                 result += binary
