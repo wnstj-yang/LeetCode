642c38641a7fa71e036b1124255185a5e2dbd4b4
양준서(JoonSeo Yang) <77601720+wnstj-yang@users.noreply.github.com>
2026-04-22 00:18:53 +0900

    Time: 15 ms (87.94%), Space: 13 MB (45.43%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.py
+++ 0087-scramble-string/0087-scramble-string.py
@@ -1,38 +1,36 @@
 class Solution(object):
     def isScramble(self, s1, s2):
         memo = {}
-        
+        # test = [1, 2, 3]
+        # print(test[:0], test[0:])
+
         def dfs(a, b):
-            # 1. 메모 확인
+            # 이미 체크한 적이 있으면 반환
             if (a, b) in memo:
                 return memo[(a, b)]
-            
-            # 2. 같으면 True
+            # 둘이 같으면 scramble 성공
             if a == b:
                 memo[(a, b)] = True
                 return True
-            
-            # 3. 문자 구성 다르면 False (가지치기)
+            # 다른 경우(구성자체가 scramble할 수 없는 경우를 체크함)
             if sorted(a) != sorted(b):
                 memo[(a, b)] = False
                 return False
-            
-            n = len(a)
-            
-            # 4. 분할 시도
-            for i in range(1, n):
-                
-                # (1) swap 안함
+
+            length = len(a)
+
+            # 빈 값이 되면 안됨, a를 기준으로
+            for i in range(1, length):
+                # 1. swap을 하지 않는다
                 if dfs(a[:i], b[:i]) and dfs(a[i:], b[i:]):
                     memo[(a, b)] = True
                     return True
-                
-                # (2) swap 함
-                if dfs(a[:i], b[n-i:]) and dfs(a[i:], b[:n-i]):
+                # 2. swap을 한다.
+                if dfs(a[:i], b[length - i:]) and dfs(a[i:], b[:length-i]):
                     memo[(a, b)] = True
                     return True
-            
+
+            # 해당안하면 끝
             memo[(a, b)] = False
             return False
-        
         return dfs(s1, s2)
